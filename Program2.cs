88d2dd5abf599c5730a09107353d25b8d1748d19
unknown <royaam@code.edu.az>
2022-12-13 01:00:05 +0400

    task2

---
+++ Program2.cs
@@ -0,0 +1,20 @@
+﻿
+
+
+
+Console.WriteLine(Test("Cavid"));
+static string Test(string name)
+{
+    string result = " ";
+    var arr = name.ToCharArray();
+
+    for (int i = name.Length - 1; i >= 0; i--)
+    {
+        result += name[i];
+
+    }
+    return result;
+
+
+
+}
